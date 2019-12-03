29fbec4baad0545051a6b95b64050a4d769851bb
Rachel-Tripp <54295282+Rachel-Tripp@users.noreply.github.com>
2019-12-03 00:17:38 -0600

    Organized Code

--- header.php
+++ header.php
@@ -45,7 +45,7 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button aria-label="menu" class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 170.77 170.77"><defs><style>.cls-1{fill:#f3e7ce;stroke:#122448;stroke-miterlimit:10;}.cls-2{fill:#122448;}</style></defs><title>Asset 7</title><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><rect class="cls-1" x="0.5" y="0.5" width="169.77" height="169.77" rx="11.15"/><rect class="cls-2" x="13.33" y="13.52" width="144" height="29.78" rx="14.89"/><rect class="cls-2" x="13.33" y="127.74" width="144" height="29.78" rx="14.89"/><rect class="cls-2" x="13.33" y="70.63" width="144" height="29.78" rx="14.89"/></g></g></svg></button>
+			<button aria-label="menu" class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><svg class="menu-btn" height="32px" id="Layer_1" style="enable-background:new 0 0 32 32;" version="1.1" viewBox="0 0 32 32" width="32px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z"/></svg></button>
 			<?php
 				wp_nav_menu( array(
 					'theme_location' => 'menu-1',
