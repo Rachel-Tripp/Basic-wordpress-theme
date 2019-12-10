a95ccdbcf9c90feaf6382bba16da1344c7ee2f6a
Rachel-Tripp <54295282+Rachel-Tripp@users.noreply.github.com>
2019-12-09 19:21:57 -0600

    javascript

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ var btns = document.querySelectorAll('.trigger');
 
 btns.forEach(function(btn) {
     btn.addEventListener('click', function() {
-        console.log('clicked');
-      btn.closest(document.querySelector('.caption').classList.toggle('open')) ; 
+    let target = btn.getAttribute('aria-controls');
+     document.querySelector('#' + target).classList.toggle('open'); 
     });
 });
